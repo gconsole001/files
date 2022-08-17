18979170be9690f2541c18a936fa0d26c3e61ece
gconsole001 <93689994+gconsole001@users.noreply.github.com>
2022-08-17 07:14:45 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-curl https://i.jpillora.com/cloud-torrent! | bash
-cloud-torrent -p 3000
+rm -rf *.gz.*
+wget "https://github.com/jpillora/cloud-torrent/releases/download/0.8.25/cloud-torrent_linux_amd64.gz" -o cloud-torrent_linux_amd64.gz
+gunzip -d cloud-torrent_linux_amd64.gz
+chmod +x cloud-torrent_linux_amd64
+./cloud-torrent_linux_amd64 -p 3000
